adbcd48271c6cc94078077d8460975b80899b7c9
Ryananta Danendra <ryanantadanendra887@gmail.com>
2025-08-14 22:30:21 +0800

    Fixing app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,12 +4,16 @@ import { createInertiaApp } from "@inertiajs/react";
 import "../css/app.css";
 import { BrowserRouter } from "react-router-dom";
 
+const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+
 createInertiaApp({
-    resolve: (name) =>
-        import(
-            `./Pages/${name}.jsx`,
-            import.meta.glob("./Pages/**/*.jsx", { eager: true })
-        ),
+    resolve: (name) => {
+        const file = `./Pages/${name}.jsx`;
+        if (!pages[file]) {
+            throw new Error(`Page not found in Vite manifest: ${file}`);
+        }
+        return pages[file];
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
         root.render(
