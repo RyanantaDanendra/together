00d5787c00b2d760be4794acb92a428f2fabb0db
Ryananta Danendra <ryanantadanendra887@gmail.com>
2025-08-15 12:32:49 +0800

    moving files from public folder

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.jsx"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.jsx",
+                "resources/js/Pages/Auth/Login.jsx",
+                "resources/js/Pages/Auth/Register.jsx",
+                "resources/js/Pages/Dashboard.jsx",
+                "resources/js/Pages/Welcome.jsx",
+            ],
             refresh: true,
         }),
         react(),
