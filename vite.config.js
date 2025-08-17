cf07472f9267d6cc3a1926937b7bb3084c71917a
Ryananta Danendra <ryanantadanendra887@gmail.com>
2025-08-17 12:42:04 +0800

    editing htaccess

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
                 "resources/js/Pages/Dashboard/Orders.jsx",
                 "resources/js/Pages/Dashboard/Payments.jsx",
                 "resources/js/Pages/Dashboard/Users.jsx",
+                "resources/js/Pages/Images.jsx",
             ],
             refresh: true,
         }),
